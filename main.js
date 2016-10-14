30ebec5937e39cd0ca8c889ff34f41ab22bc42bc
Amanda Thorne <manda.thorne@gmail.com>
2016-10-14 18:37:43 -0500

    playing with pseudo elements

---
+++ main.js
@@ -0,0 +1,10 @@
+// let canvas = document.getElementById('canvas')
+// let ctx = canvas.getContext('2d');
+//
+// function drawPumpkin() {
+//   ctx.beginPath();
+//   ctx.arc(x, y, 50, 25, 0, Math.PI * 2, false);
+//   ctx.fillStyle = orange;
+//   ctx.fill();
+//   ctx.closePath();
+// }
